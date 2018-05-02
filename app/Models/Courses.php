97f40575e57a8becf052a97078fcb90294754e90
liuyongbing <mike.doon.lau@gmail.com>
2018-05-02 16:02:29 +0800

    API: Courses

--- app/Models/Courses.php
+++ app/Models/Courses.php
@@ -2,7 +2,29 @@
 
 namespace App\Models;
 
+use App\Constants\Dictionary;
+use App\Helpers\FileHelper;
+
 class Courses extends BasicModel
 {
-    //
+    protected $appends = ['status_desc', 'image_url'];
+    
+    /**
+     * 图片:完整Url
+     *
+     * @return string
+     */
+    public function getImageUrlAttribute() {
+        return FileHelper::fileUrl($this->image, Dictionary
+                ::FILE_TYPE['COURSEWARE']);
+    }
+    
+    /**
+     * 状态:文本
+     *
+     * @return string
+     */
+    public function getStatusDescAttribute() {
+        return trans('attributes.grades.status.' . $this->status);
+    }
 }
