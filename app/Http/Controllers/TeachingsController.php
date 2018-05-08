42c9e5feea1975699e78c233d6a5ef87d1418215
liuyongbing <mike.doon.lau@gmail.com>
2018-05-08 10:11:12 +0800

    API: Teachings

--- app/Http/Controllers/TeachingsController.php
+++ app/Http/Controllers/TeachingsController.php
@@ -2,12 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Constants\Dictionary;
 use App\Repositories\TeachingsRepository;
+use Illuminate\Http\Request;
 
 class TeachingsController extends Controller
 {
     public function init()
     {
         $this->repository = new TeachingsRepository();
+    }
+    
+    /**
+     * 列表
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function index(Request $request)
+    {
+        $params = [];
+        
+        $courseId = $request->input('course_id');
+        if (!empty($courseId))
+        {
+            $params['course_id'] = $courseId;
+        }
+        
+        $classNumber = $request->input('class_number');
+        if (!empty($classNumber))
+        {
+            $params['class_number'] = $classNumber;
+        }
+        
+        $page = $request->input('page', 1);
+        $size = $request->input('size', Dictionary::PAGE_SIZE);
+        $offset = (int)($page-1) * $size;
+        
+        $order = $request->input('order', '');
+        
+        $result = $this->repository->list($params, $offset, $size, $order);
+        return $this->response($result);
     }
 }
