fb45858011a2b4ff5da4d6d11d08a9c884d3b64f
liuyongbing <mike.doon.lau@gmail.com>
2018-05-29 11:37:16 +0800

    API:Category

--- app/Repositories/CategoriesRepository.php
+++ app/Repositories/CategoriesRepository.php
@@ -9,4 +9,29 @@ class CategoriesRepository extends Repository
     {
         $this->model = new Categories();
     }
+    
+    /**
+     * 列表
+     *
+     * @param array $params
+     * @return array
+     */
+    public function list($conditions, $offset = 0, $limit = 10, $order = [])
+    {
+        $result = parent::list($conditions, $offset, $limit, $order);
+        
+        $data = [];
+        if (!empty($result['list']))
+        {
+            foreach ($result['list'] as $item)
+            {
+                $data[$item['id']] = $item;
+            }
+        }
+        
+        return [
+            'total' => $result['total'],
+            'list' => $data
+        ];
+    }
 }
