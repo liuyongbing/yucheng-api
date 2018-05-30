1babf400560803587387abf5866e163a18778ab7
liuyongbing <mike.doon.lau@gmail.com>
2018-05-30 23:50:56 +0800

    API: Apply

--- app/Models/Apply.php
+++ app/Models/Apply.php
@@ -2,7 +2,45 @@
 
 namespace App\Models;
 
+use App\Constants\Dictionary;
+
 class Apply extends BasicModel
 {
     protected $hidden = ['updated_at'];
+    
+    protected $appends = ['brand', 'budget', 'status_desc'];
+    
+    /**
+     * 状态:文本
+     *
+     * @return string
+     */
+    public function getStatusDescAttribute()
+    {
+        return trans('attributes.grades.status.' . $this->status);
+    }
+    
+    /**
+     * 品牌:文本
+     *
+     * @return string
+     */
+    public function getBrandAttribute()
+    {
+        $brand = Dictionary::BRAND;
+        
+        return isset($brand[$this->brand_id]) ? $brand[$this->brand_id] : '';
+    }
+    
+    /**
+     * 预算:文本
+     *
+     * @return string
+     */
+    public function getBudgetAttribute()
+    {
+        $budget = Dictionary::BUDGET;
+        
+        return isset($budget[$this->budget_range]) ? $budget[$this->budget_range] : '';
+    }
 }
