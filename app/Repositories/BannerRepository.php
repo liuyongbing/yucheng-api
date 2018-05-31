59d8d354538a12019458fcac8cb14038e9f3f8fe
liuyongbing <mike.doon.lau@gmail.com>
2018-05-31 23:32:43 +0800

    API: Banner

--- app/Repositories/BannerRepository.php
+++ app/Repositories/BannerRepository.php
@@ -9,4 +9,27 @@ class BannerRepository extends Repository
     {
         $this->model = new Banner();
     }
+    
+    /**
+     * 新增
+     *
+     * @param array $data
+     * @return \App\Models\BasicModel
+     */
+    public function store($data)
+    {
+        $item = $this->model;
+        
+        $item->position_id  = isset($data['position_id']) ? $data['position_id'] : '';
+        $item->target       = isset($data['target']) ? $data['target'] : '';
+        $item->url          = isset($data['url']) ? $data['url'] : '';
+        $item->title        = isset($data['title']) ? $data['title'] : '';
+        $item->image        = isset($data['image']) ? $data['image'] : '';
+        $item->sort         = (int)$data['sort'];
+        $item->status       = isset($data['status']) ? (int)$data['status'] : 0;
+        
+        $item->save();
+        
+        return $item;
+    }
 }
