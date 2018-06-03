2dab329a9ef3029d829fa2cc62dc7c88c88a2799
liuyongbing <mike.doon.lau@gmail.com>
2018-06-03 21:47:40 +0800

    API: Apply

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'accounts*',
+        'apply*',
         'attachment/upload/*',
         'branches*',
         'courses*',
--- app/Repositories/ApplyRepository.php
+++ app/Repositories/ApplyRepository.php
@@ -21,86 +21,20 @@ class ApplyRepository extends Repository
     {
         $item = $this->model;
         
-        $item->category_id  = (int)$data['category_id'];
-        $item->branch_id    = isset($data['branch_id']) ? (int)$data['branch_id'] : 0;
-        $item->title        = !empty($data['title']) ? $data['title'] : '';
-        $item->contents     = !empty($data['contents']) ? NewsHelper::inputContents($data['contents']) : '';
-        $item->summary      = $this->formatSummary($item->contents);
-        $item->show_year    = $this->formatShowYear();
-        $item->sort         = (int)$data['sort'];
-        $item->status       = 1;
+        $item->brand_id = (int)$data['brand_id'];
+        $item->username = !empty($data['username']) ? $data['username'] : '';
+        $item->mobile = !empty($data['mobile']) ? $data['mobile'] : '';
+        $item->budget_range = isset($data['budget_range']) ? (int)$data['budget_range'] : 0;
+        $item->province = !empty($data['province']) ? $data['province'] : '';
+        $item->city = !empty($data['city']) ? $data['city'] : '';
+        $item->area = !empty($data['area']) ? $data['area'] : '';
+        $item->address = !empty($data['address']) ? $data['address'] : '';
+        $item->summary = !empty($data['summary']) ? $data['summary'] : '';
+        $item->remark = '';
+        $item->status = 1;
         
         $item->save();
         
         return $item;
-    }
-    
-    /**
-     * 修改
-     *
-     * @param int $id
-     * @param array $data
-     * @return \App\Models\BasicModel
-     */
-    public function update($id, $data = [])
-    {
-        $item = $this->model->find($id);
-        $attributes = $item->getAttributes();
-        
-        $data['contents'] = NewsHelper::inputContents($data['contents']);
-        $data['summary'] = $this->formatSummary($data['contents']);
-        
-        foreach ($data as $key => $value)
-        {
-            if (key_exists($key, $attributes))
-            {
-                $item->$key = $value;
-            }
-        }
-        
-        $item->save();
-        
-        return $item;
-    }
-    
-    public function years()
-    {
-        $data = [];
-        $items = $this->model->select('show_year')->distinct()->get();
-        foreach ($items as $item)
-        {
-            $data[$item->show_year] = $item->show_year;
-        }
-        rsort($data);
-        
-        return $data;
-    }
-    
-    public function next($id)
-    {
-        $item = $this->model->where('id', '>', $id)
-                            ->where('status', '=', 1)
-                            ->first();
-        
-        return $item;
-    }
-    
-    public function previous($id)
-    {
-        $item = $this->model->where('id', '<', $id)
-                            ->where('status', '=', 1)
-                            ->first();
-        
-        return $item;
-    }
-    
-    protected function formatSummary($contens)
-    {
-        return '';
-    }
-    
-    protected function formatShowYear()
-    {
-        return date('Y');
     }
 }
