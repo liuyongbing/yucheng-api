d5b9da7e5e1328d016d3ac85bb9e6a96b3fbd3a8
liuyongbing <mike.doon.lau@gmail.com>
2018-07-21 11:28:09 +0800

    develop_ppt

--- app/Models/Coursewares.php
+++ app/Models/Coursewares.php
@@ -13,6 +13,7 @@ class Coursewares extends BasicModel
         'course_name',
         'file_music_url',
         'file_ppt_url',
+        'file_ppt_online_url',
         'file_video_url',
         'status_desc',
     ];
@@ -55,6 +56,19 @@ class Coursewares extends BasicModel
     public function getFilePptUrlAttribute()
     {
         return FileHelper::fileUrl($this->file_ppt, Dictionary::FILE_TYPE['COURSEWARE']);
+    }
+    
+    /**
+     * 课件PPT:完整Url
+     *
+     * @return string
+     */
+    public function getFilePptOnlineUrlAttribute()
+    {
+        //http://ow365.cn/?i=16363&furl=http://file.100yjy.com/demo.pptx
+        return env('OFFICE_WEB_SITE', 'http://ow365.cn/') . '?i=' . 
+               env('OFFICE_WEB_ID', '16363') . '&furl=' . 
+               $this->file_ppt_url;
     }
     
     /**
