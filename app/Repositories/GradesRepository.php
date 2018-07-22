e87e28a1cdbb1519bd95834025c3e2b91a409cb5
liuyongbing <mike.doon.lau@gmail.com>
2018-07-22 16:39:41 +0800

    develop_ppt

--- app/Repositories/GradesRepository.php
+++ app/Repositories/GradesRepository.php
@@ -9,4 +9,25 @@ class GradesRepository extends Repository
     {
         $this->model = new Grades();
     }
+    
+    /**
+     * 新增
+     *
+     * @param array $data
+     * @return \App\Models\BasicModel
+     */
+    public function store($data)
+    {
+        $item = $this->model;
+        
+        $item->brand_id  = !empty($data['brand_id']) ? (int)$data['brand_id'] : 0;
+        $item->title     = !empty($data['title']) ? $data['title'] : '';
+        $item->image     = !empty($data['image']) ? $data['image'] : '';
+        $item->sort      = (int)$data['sort'];
+        $item->status    = 1;
+        
+        $item->save();
+        
+        return $item;
+    }
 }
