8cb43a1d251e88337a4016c6c73fcd1cc973ed19
liuyongbing <mike.doon.lau@gmail.com>
2018-08-01 09:24:57 +0800

    develop_ppt UPLOAD_FILE_FOLDER

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,7 +51,7 @@ return [
         'public' => [
             'driver' => 'local',
             //'root' => storage_path('app/public'),
-            'root' => env('UPLOAD_FILE_FOLDER'),
+            'root' => env('UPLOAD_FILE_FOLDER', storage_path('app/public')),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
