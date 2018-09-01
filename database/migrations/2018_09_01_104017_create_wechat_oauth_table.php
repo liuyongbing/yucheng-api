9ca6db55791867f39f96016f9090a5db7e5d1f3d
liuyongbing <mike.doon.lau@gmail.com>
2018-09-01 11:00:25 +0800

    migrations:create_wechat_oauth_table

---
+++ database/migrations/2018_09_01_104017_create_wechat_oauth_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWechatOauthTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wechat_oauth', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('openid', 28)->default('');
+            $table->json('original')->default('');
+            $table->dateTime('created_at')->nullable();
+            $table->dateTime('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wechat_oauth');
+    }
+}
